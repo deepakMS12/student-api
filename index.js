e092b15403caafb98289c8f4cd03758d4ec4be91
deepakMS12 <deepak.dhiman@mscorpres.in>
2026-06-16 16:35:07 +0530

    changes

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/msg", (req, res) => {
+  res.json({
+    message: "Student API Working"
+  });
+});
+app.listen(3000, () => {
+  console.log("Server running");
+});
+
+module.exports = app;
